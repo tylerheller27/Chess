e03f5381a9dff610a03bb77c33d7b12818762f7a
Tyler Heller <tylerheller27@gmail.com>
2026-01-19 19:53:53 -0500

    adding more files

---
+++ Simple Chess/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.*; //
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Simple Chess");// simple chess will be displayed on the top of windows address bar
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);// you cannot resize window of program
+        window.setLocationRelativeTo(null); // starts program in the center of your screen
+        window.setVisible(true); // makes so that the window will actually appear
+        //test
+
+    }//main
+
+}//class
