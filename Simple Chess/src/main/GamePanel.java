fe83223fd3a79ea0ea7898cf5df04ef50f30c791
Tyler Heller <tylerheller27@gmail.com>
2026-01-25 18:42:28 -0500

    created game loop

--- Simple Chess/src/main/GamePanel.java
+++ Simple Chess/src/main/GamePanel.java
@@ -3,18 +3,63 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel // JPanel class we want to customize it.
+public class GamePanel extends JPanel implements Runnable // JPanel class we want to customize it.
                                       // where going to use this as the game screen
 {
 
   public static final int WIDTH = 1100;
   public static final int HEIGHT = 800;
+  final int FPS = 60;
+  Thread gameThread;
 
-    public GamePanel()
+    public GamePanel() // on arg constructor. when an obj is created in main it will be created with
         {
-      setPreferredSize(new Dimension(WIDTH,HEIGHT));
-      setBackground(Color.black);
+          setPreferredSize(new Dimension(WIDTH,HEIGHT));
+         setBackground(Color.black);
         }
+
+        public void launchGame()
+        {
+          gameThread = new Thread(this);
+          gameThread.start();
+        }
+        @Override
+        public void run()
+        {
+          //gmae loop
+          double drawInterval = 1000000000/FPS;
+          double delta = 0;
+          long lastTime = System.nanoTime();
+          long currentTime;
+
+          currentTime =  System.nanoTime();
+
+          delta += (currentTime - lastTime)/drawInterval;
+          lastTime = currentTime;
+
+                  if(delta >=1)
+                  {
+                    update();
+                    repaint();
+                    delta--;
+                  }//if
+        }//void override
+
+        //methods
+        // the purpose of these to methods is to create a game loop that keeps calling these to methods 60 times per
+        // second to continously update the screen
+        public void update()
+        {
+
+        }
+        /*
+        paintComponent is a method in JComponent that JPanel inherits and is used to draw objects on tlhe panel.
+        */
+        public void paintComponent(Graphics g)
+        {
+          super.paintComponent(g);
+        }
+
 
 
 
--- Simple Chess/src/main/Main.java
+++ Simple Chess/src/main/Main.java
@@ -16,9 +16,11 @@ public class Main {
 
         window.setLocationRelativeTo(null); // starts program in the center of your screen
         window.setVisible(true); // makes so that the window will actually appear
-        //test
+
+        gp.launchGame();
+
 		
-		//test git comment
+
 
     }//main
 
